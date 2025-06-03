9cee7e42c20a247e6fcc76bfe84d25db721070b3
Michael107930 <myamidsuarez@ucundinamarca.edu.co>
2025-06-03 10:22:08 -0500

    Subiendo Prueba

--- netlify/functions/asistencia.js
+++ netlify/functions/asistencia.js
@@ -3,7 +3,7 @@ var cors = require("cors");
 var serverless = require ('serverless-http');
 var port = process.env.PORT || 5000;
 var app = express();
-var estudiantesroutes = require("../../backend/routes/asistenciaroutes.js");
+var estudiantesroutes = require("../../backend/controllers/asistenciacontrollers");
 app.use(express.json());
 app.use(cors());
  
